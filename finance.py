494393914d58f2cd95ff8b81ae77710b6fb00663
Atsushi <cdglun.soatspgmail.com>
2022-12-30 23:15:18 +0900

    2nd commit

---
+++ finance.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+days = 20
+tickers = {
+    'apple': 'AAPL'
+    'facebook': 'FB'
+    'google': 'GOOGL'
+    'microsoft': 'MSFT'
+    'netflix': 'NFLX'
+    'amazon': 'AMZN'
+}
+
+def get_data(days, tickers):
+    df = pd.DataFrame()
+    for company in tickers.keys():
+    #company = 'aaple'
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f'{days}d')
+        hist.index = hist.index.strftime('%d %B %Y')
+        hist = hist[['Close']]
+        hist.columns = ['company']
+        hist = hist.T
+        hist.index.name = 'Name'
+        df = pd.concat([df, hist])
+    return df
